6d0b5a01c608b14d7222681253e7a3716667df89
Bundula Patrik <patrik.bundula@gammaorg.hu>
2024-03-22 15:31:25 +0100

    get blob methods added

--- Logic/Services/BlobService.cs
+++ Logic/Services/BlobService.cs
@@ -1,5 +1,4 @@
 ﻿using Azure.Storage.Blobs;
-using Microsoft.AspNetCore.Mvc;
 using Models.Contracts.V1.Blob;
 using Models.Interfaces;
 
@@ -7,17 +6,38 @@ namespace Logic.Services
 {
     public class BlobService : IBlobService
     {
+        private readonly BlobServiceClient _blobServiceClient;
+        private const string _blobContainerName = "blogs-image";
+        public BlobService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+
+        }
+        public async Task<BlobInfo> GetBlobAsync(string name)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_blobContainerName);
+            var blobClient = containerClient.GetBlobClient(name);
+            var blobDownloadInfo = await blobClient.DownloadContentAsync();
+            Stream contentStream = blobDownloadInfo.Value.Content.ToStream();
+            return new BlobInfo(contentStream, blobDownloadInfo.Value.Details.ContentType);
+        }
+        public async Task<IEnumerable<string>> ListBlobsAsync()
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_blobContainerName);
+            var items = new List<string>();
+
+            await foreach (var item in containerClient.GetBlobsAsync())
+            {
+                items.Add(item.Name);
+            }
+            return items;
+        }
+        public Task UploadFileBlobAsync(string filePath, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task DeleteBlobAsync(string blobName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<BlobInfo> GetBlobAsync(string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<string>> ListBlobsAsync()
         {
             throw new NotImplementedException();
         }
@@ -27,9 +47,5 @@ namespace Logic.Services
             throw new NotImplementedException();
         }
 
-        public Task UploadFileBlobAsync(string filePath, string fileName)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
